30fdccd9b0a3da234101198e4d7c8958d61b6b48
Sergei Palmer <zigpalmer@gmail.com>
2021-09-16 17:43:13 +0100

    Created a test for number = 5, added if control flow to fizzbuzz.rb

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,10 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-end 
+end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when pass 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
